0596745df7770a0be56039d7fdb321008ecce308
khalil ladjal <khalilladjal@gmail.com>
2021-07-06 01:31:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { UserSession, AppConfig } from "blockstack"
+import Login from "./Login"
+import TodoList from "./TodoList"
+
+const appConfig = new AppConfig(["store_write"])
+const userSession = new UserSession({ appConfig: appConfig })
+
+
+class App extends React.Component {
+  UNSAFE_componentWillMount() {
+    if (userSession.isSignInPending()) {
+      userSession
+        .handlePendingSignIn()
+        .then(() => {
+          window.location = window.location.origin;
+        })
+        .catch(err => console.log(err));
+    }
+  }
+  render() {
+    return (
+      <div>
+        {userSession.isUserSignedIn() ? (
+          <TodoList userSession={userSession} />
+        ) : (
+          <Login userSession={userSession} />
+        )}
+      </div>
+    )
+  }
+}
+
+
+export default App
